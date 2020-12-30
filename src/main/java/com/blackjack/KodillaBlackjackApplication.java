0ed1cb32b100f9fab5a819961db7de9257fab7ee
Dariusz Grala <gralusek@interia.pl>
2020-12-30 18:38:04 +0100

    First Commit

---
+++ src/main/java/com/blackjack/KodillaBlackjackApplication.java
@@ -0,0 +1,13 @@
+package com.blackjack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaBlackjackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaBlackjackApplication.class, args);
+    }
+
+}
